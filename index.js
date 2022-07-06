0aa474f92f8daee7feb1efdaf844dbe58759841a
lukis76 <kiu.manoguerra@gmail.com>
2022-07-06 13:55:40 -0300

    :p

---
+++ index.js
@@ -0,0 +1,94 @@
+const tasks = []
+let time = 0 
+let timer = null
+let timeBreak = null
+let current = null
+
+
+const bAdd = document.querySelector('#b_add')
+const inputTask = document.querySelector('#input_task')
+const from = document.querySelector('#from')
+const taskName = document.querySelector("#time #taskName")
+const timeDiv = document.querySelector('#time #value')
+const tasksContainer = document.querySelector('#tasks')
+const startButtons = document.querySelectorAll('.task .start_button')
+
+
+from.addEventListener('submit', (e) => {
+  e.preventDefault()
+  if(inputTask.value !== ''){
+    createTask(inputTask.value)
+    inputTask.value = ''
+    renderTask()
+  }
+})
+
+const createTask = (value) => {
+  const newTask = {
+    id: (Math.random() * 100).toString(36).slice(3), //id dinamico
+    title: value,
+    completed: false,
+  }
+
+  tasks.unshift(newTask)
+
+}
+
+const renderTask = () => {
+  const html = tasks.map(task => {
+    return `
+    <div class="task>
+      <div class="completed">
+      ${task.completed 
+        ?`<span class="done" >Done</span>` 
+        : `<button class="start_button" data-id="${task.id}" >start</button>` }
+      </div>
+      <div class="title">${task.title}</div>
+    </div>
+    `
+  })
+
+  tasksContainer.innerHTML = html.join('')
+
+
+  startButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      if(!timer){
+        const id = button.getAttribute('data-id')
+        startButtonHandler(id)
+        button.textContent = 'In progress...'
+      }
+    })
+  })
+}
+
+const startButtonHandler = (id) => {
+  time = 25 * 60
+  current = id
+  const taskIndex = tasks.findIndex(task => task.id === id)
+  taskName.textContent = tasks[taskIndex].title
+  timer = setInterval(() => {
+    timeHandler(id)
+  },1000)
+}
+
+const timeHandler = (id) => {
+  time--
+  renhderTime()
+
+  if(time === 0){
+    clearInterval(timer)
+    current = null
+    taskName.textContent = ''
+    renhderTime()
+  }
+}
+
+const renhderTime = () => {
+  const minutes = parseInt(time / 60)
+  const seconds = parseInt(time % 60)
+
+
+timeDiv.textContent = `${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}`
+}
+
